1bb3b6d80284765c6dd3eadf2d63895919612fe0
ghano <ghanoasx@gmail.com>
2024-10-08 00:53:29 +0100

    fix(authentication): add user proper abilities to newly registered users

--- app/Http/Controllers/v1/Auth/RegisteredUserController.php
+++ app/Http/Controllers/v1/Auth/RegisteredUserController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
 use App\Http\Resources\v1\UserResource;
 use App\Http\Requests\v1\Auth\RegisterUserRequest;
+use App\Services\UserService;
 
 class RegisteredUserController extends Controller
 {
@@ -18,28 +19,32 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(RegisterUserRequest $request)
-    {
+    public function store(
+        RegisterUserRequest $request,
+        UserService $userService
+    ): UserResource {
         $auth = User::user();
 
         if (!$auth || !$auth->can(AbilityEnum::CREATE->value, User::class)) {
-            $this->failedAsNotFound('workspace');
+            $this->failedAsNotFound("user");
         }
 
         $data = $request->validated();
-        $username = User::USERNAME_BASE . '_' . Str::random(8);
+        $username = User::USERNAME_BASE . "_" . Str::random(8);
         $slug = $this->getSlug($username);
 
         $user = User::query()->create([
-            'first_name' => $data['first_name'],
-            'last_name' => $data['last_name'],
-            'slug' => $slug,
-            'username' => $username,
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            "first_name" => $data["first_name"],
+            "last_name" => $data["last_name"],
+            "slug" => $slug,
+            "username" => $username,
+            "email" => $data["email"],
+            "password" => Hash::make($data["password"]),
         ]);
 
         event(new Registered($user));
+
+        $userService->getUserCapabilitiesForUser($auth, $user);
 
         return new UserResource($user);
     }
